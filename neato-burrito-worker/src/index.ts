1c35068fc2f47f7d12eb27a4780e3ee68f42e7b9
Barrett Routon <barrett@binderpos.com>
2023-09-27 11:24:11 -0500

    feat: add durable object to worker

--- neato-burrito-worker/src/index.ts
+++ neato-burrito-worker/src/index.ts
@@ -13,7 +13,40 @@ import { sendDiscordMessage } from "./sendDiscordMessage";
 export interface Env {
 	DISCORD_HOOK: string;
 	CLIENT_SECRET: string;
+	PLAYER_STATE: DurableObjectNamespace;
 }
+
+type PlayerRegistration = {
+	email: string;
+	name: string;
+};
+
+export const registerPlayer = async (request: Request, env: Env, ctx: ExecutionContext): Promise<Response> => {
+	const data = await request.json();
+	const systemId = request.headers.get("x-neato-burrito-system-id");
+	if (!systemId) {
+		return new Response("Missing system id", { status: 400 });
+	}
+	const { email, name } = data as PlayerRegistration;
+	if(!email || !name) {
+		return new Response("Missing email or name", { status: 400 });
+	}
+	let id = env.PLAYER_STATE.idFromName(systemId);
+	let playerState = env.PLAYER_STATE.get(id);
+	const res = await playerState.fetch(new Request(request, {
+		method: "POST",
+		headers: {
+			"content-type": "application/json;charset=UTF-8",
+		},
+		body: JSON.stringify({ email, name }),
+	}));
+	const body = await res.json();
+	return new Response(JSON.stringify(body), {
+		headers: {
+			"content-type": "application/json;charset=UTF-8",
+		},
+	});
+};
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
@@ -21,9 +54,13 @@ export default {
 		if (incomingSecret !== env.CLIENT_SECRET) {
 			return new Response("Unauthorized", { status: 401 });
 		}
+		const path = new URL(request.url).pathname;
+		if (path == "/register-player") {
+			return registerPlayer(request, env, ctx);
+		}
 		const data = await request.json();
 		console.log(JSON.stringify(data, null, 3));
-		const messages = (data as any).filter(t => t.value).map(d => {
+		const messages = (data as any).filter((t: any) => t.value).map((d: any) => {
 			const message = {
 				embeds: [
 					{
